b079933ad01d6fc5b6fe16431987a157987468d6
vladimirmitnovitskiy <harrellprmime@gmail.com>
2026-05-28 00:32:33 +0300

    add Market

--- src/main/kotlin/application/Market.kt
+++ src/main/kotlin/application/Market.kt
@@ -1,2 +1,18 @@
 package application
 
+import domain.Establishment
+
+class Market(initialCards: List<Establishment>) {
+    // Внутренний изменяемый список, наружу отдаем неизменяемый (инкапсуляция)
+    private val _availableCards = initialCards.toMutableList()
+    val availableCards: List<Establishment> get() = _availableCards.toList()
+
+    fun removeCard(card: Establishment) {
+        _availableCards.remove(card)
+    }
+
+    // В будущем здесь можно добавить логику пополнения рынка из колоды
+    fun replenishMarket() {
+        // Пока оставим пустым
+    }
+}

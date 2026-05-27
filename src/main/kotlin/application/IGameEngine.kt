dc284f0579246b1c1f73a9d4547def2613362627
vladimirmitnovitskiy <harrellprmime@gmail.com>
2026-05-28 00:32:12 +0300

    add IGameEngine

--- src/main/kotlin/application/IGameEngine.kt
+++ src/main/kotlin/application/IGameEngine.kt
@@ -1,2 +1,22 @@
 package application
 
+import domain.*
+import kotlinx.coroutines.flow.StateFlow
+
+// Состояние игры (будет автоматически обновлять GUI во 2-м задании)
+data class GameState(
+    val activePlayer: Player,
+    val players: List<Player>,
+    val market: Market,
+    val lastDiceRoll: Int = 0,
+    val winner: Player? = null
+)
+
+interface IGameEngine {
+    val stateFlow: StateFlow<GameState>
+
+    fun rollDice()
+    fun buyEstablishment(card: Establishment)
+    fun buildLandmark(landmark: Landmark)
+    fun abortGame()
+}

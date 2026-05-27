1684a75ce311b3bbd291a0a2e0ae49aace36066e
vladimirmitnovitskiy <harrellprmime@gmail.com>
2026-05-28 00:31:44 +0300

    add Dice class

--- src/main/kotlin/application/Dice.kt
+++ src/main/kotlin/application/Dice.kt
@@ -1,2 +1,21 @@
 package application
 
+// Интерфейс генератора случайных чисел (виртуализация из твоего ТЗ)
+interface Dice {
+    fun roll(): Int
+}
+
+// Реальный кубик для обычной игры
+class RandomDice : Dice {
+    override fun roll(): Int = (1..6).random()
+}
+
+// Подставной кубик для тестов (выдает заранее заданные числа)
+class FakeDice(vararg val fixedRolls: Int) : Dice {
+    private var index = 0
+    override fun roll(): Int {
+        val result = fixedRolls[index % fixedRolls.size]
+        index++
+        return result
+    }
+}

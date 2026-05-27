a3b32ebd30b06359734bd9d0f6fc996f33580a86
vladimirmitnovitskiy <harrellprmime@gmail.com>
2026-05-28 00:37:33 +0300

    add Player class

---
+++ src/main/kotlin/domain/Player.kt
@@ -0,0 +1,30 @@
+package domain
+
+import java.util.UUID
+
+class Player(
+    val id: UUID = UUID.randomUUID(),
+    val name: String,
+    var balance: Int = 3 // По правилам на старте дают 3 монеты
+) {
+    // Карты, которые уже построил игрок
+    val establishments = mutableListOf<Establishment>()
+    val landmarks = mutableListOf<Landmark>()
+
+    fun addCoins(amount: Int) {
+        if (amount > 0) balance += amount
+    }
+
+    // Возвращает, сколько реально монет удалось списать
+    // (по правилам баланс не может стать меньше 0)
+    fun deductCoins(amount: Int): Int {
+        val actualDeduction = if (balance >= amount) amount else balance
+        balance -= actualDeduction
+        return actualDeduction
+    }
+
+    // Проверка победы: если построено 4 достопримечательности
+    fun hasWon(): Boolean {
+        return landmarks.size >= 4
+    }
+}

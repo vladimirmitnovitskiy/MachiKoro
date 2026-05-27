79a5f7b5da4c8d4ce4453b5a92f7cdacc33fbfe8
vladimirmitnovitskiy <harrellprmime@gmail.com>
2026-05-28 02:43:07 +0300

    make deck refresh

--- src/main/kotlin/application/Market.kt
+++ src/main/kotlin/application/Market.kt
@@ -2,17 +2,34 @@ package application
 
 import domain.Establishment
 
-class Market(initialCards: List<Establishment>) {
-    // Внутренний изменяемый список, наружу отдаем неизменяемый (инкапсуляция)
-    private val _availableCards = initialCards.toMutableList()
+class Market(initialDeck: List<Establishment>) {
+
+    // Закрытая колода (перемешиваем при создании)
+    private val deck = initialDeck.toMutableList().apply { shuffle() }
+
+    // Открытые карты на столе (доступные для покупки)
+    private val _availableCards = mutableListOf<Establishment>()
     val availableCards: List<Establishment> get() = _availableCards.toList()
+
+    init {
+        // При создании игры сразу выкладываем стартовые карты на стол
+        replenishMarket()
+    }
 
     fun removeCard(card: Establishment) {
         _availableCards.remove(card)
+        // Срабатывает автоматическое пополнение резерва!
+        replenishMarket()
     }
 
-    // В будущем здесь можно добавить логику пополнения рынка из колоды
+    // Логика автоматического пополнения
     fun replenishMarket() {
-        // Пока оставим пустым
+        // Допустим, на столе всегда должно лежать 5 карт (или меньше, если колода опустела)
+        val maxMarketSize = 5
+
+        while (_availableCards.size < maxMarketSize && deck.isNotEmpty()) {
+            val drawnCard = deck.removeAt(0) // Берем верхнюю карту из колоды
+            _availableCards.add(drawnCard)   // Кладем на стол
+        }
     }
 }

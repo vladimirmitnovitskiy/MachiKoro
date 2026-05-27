8916cea62f20c8b1b9e338aaf0da74875144030a
vladimirmitnovitskiy <harrellprmime@gmail.com>
2026-05-28 00:41:38 +0300

    add build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,13 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    // Корутины нужны для StateFlow в движке игры
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+
+    // Библиотеки для написания тестов (JUnit 5 + MockK)
+    testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    testImplementation("io.mockk:mockk:1.13.10")
 }
 
 kotlin {
